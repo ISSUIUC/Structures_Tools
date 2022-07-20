a2cd9d06d421337e1a53aeda80ce878a871723fd
Gautam Dayal <gautam.dayal@yahoo.com>
2022-07-20 11:03:21 -0500

    Added class inits

--- ork-scanner/rocket_components.py
+++ ork-scanner/rocket_components.py
@@ -2,17 +2,29 @@
 Rocket Component Objects
 
 A few objects that should make the parsing cleaner than reading out of a
-dictionary every time
+dictionary every time.
 """
 
 class Nosecone(object):
-    pass
+    def __init__(self, length, geometry, material):
+        self.length = length
+        self.geometry = geometry
+        self.material = material
 
 class Finset(object):
-    pass
+    def __init__(self, fin_count, height, root_chord, tip_chord, material):
+        self.fin_count = fin_count
+        self.height = height
+        self.root_chord = root_chord
+        self.tip_chord = tip_chord
+        self.material = material
 
 class Simulation(object):
-    pass
+    def __init__(self, apogee, max_velocity, max_acceleration, flight_time):
+        self.apogee = apogee
+        self.max_velocity = max_velocity
+        self.max_acceleration = max_acceleration
+        self.flight_time = flight_time
 
 class BodyTube(object):
     pass
