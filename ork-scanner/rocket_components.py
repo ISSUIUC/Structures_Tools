601e8edbc2871269a34b7eccefed569207de5411
Gautam Dayal <gautam.dayal@yahoo.com>
2022-07-25 00:42:39 -0500

    Added str methods to rocket components

--- ork-scanner/rocket_components.py
+++ ork-scanner/rocket_components.py
@@ -41,6 +41,8 @@ class Nosecone:
         self.length = length
         self.geometry = geometry
         self.material = material
+    def __str__(self):
+        return f"Length:{self.length},Geometry:{self.geometry},Material:{self.material}"
 
 class Finset:
     def __init__(self, fin_count, height, root_chord, tip_chord, material):
@@ -49,6 +51,8 @@ class Finset:
         self.root_chord = root_chord
         self.tip_chord = tip_chord
         self.material = material
+    def __str__(self):
+        return f"Fin Count:{self.fin_count},Height:{self.height},Root Chord:{self.root_chord},"
 
 class Simulation:
     def __init__(self, apogee, max_velocity, max_acceleration, flight_time):
@@ -57,15 +61,19 @@ class Simulation:
         self.max_acceleration = max_acceleration
         self.flight_time = flight_time
     def __str__(self):
-        return "Apogee: " + str(self.apogee) + ",Max Velocity: " + str(self.max_velocity) + ",Max Acceleration: " + str(self.max_acceleration) + ",Flight Time: " + str(self.flight_time)
+        return f"Apogee:{self.apogee},Max Velocity:{self.max_velocity},Max Acceleration:{self.max_acceleration}"
 
 class BodyTube:
     def __init__(self, length, material, thickness):
         self.length = length
         self.material = material
         self.thickness = thickness
+    def __str__(self):
+        return f"Length:{self.length},Material:{self.material},Thickness:{self.thickness}"
 
 class RecoverySystem:
     def __init__(self, shockcord_length, chute_material):
         self.shockcord_length = shockcord_length
         self.chute_material = chute_material
+    def __str__(self):
+        return f"Shockcord Length:{self.shockcord_length},Chute Material:{self.chute_material}"
